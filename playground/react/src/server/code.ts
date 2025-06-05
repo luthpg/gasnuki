902f29266e24dfb8b720f534507cd4a00efd6f91
luth <88308177+luthpg@users.noreply.github.com>
2025-06-05 14:04:31 +0900

    `generate.ts`での型定義生成ロジックを改善

--- playground/react/src/server/code.ts
+++ playground/react/src/server/code.ts
@@ -1,3 +1,18 @@
 function myFunction() {
   return 'Hello world!';
 }
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function test<T extends string | number>(x: T): Person {
+  return typeof x === 'string' ? {
+    age: 15,
+    name: x
+  } : {
+    age: x,
+    name: 'John'
+  }
+};
