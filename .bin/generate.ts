408eba343f34ca7d6cca1e9659aef46c70dcf5bf
luth <88308177+luthpg@users.noreply.github.com>
2025-07-24 10:40:55 +0000

    fix file path, add testing to npm-prepare

--- .bin/generate.ts
+++ .bin/generate.ts
@@ -1,7 +1,7 @@
 import * as fs from 'node:fs/promises';
 
 (async () => {
-  const file = await fs.readFile('./bin/typedef/clientside.d.ts', 'utf-8');
+  const file = await fs.readFile('./.bin/typedef/clientside.d.ts', 'utf-8');
   await fs.writeFile(
     './src/modules/clientside.json',
     JSON.stringify({
